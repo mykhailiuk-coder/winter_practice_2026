9850cc76d7ee6366698366f912123208b7c878dd
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-02-12 16:25:25 +0200

    Add LoginForm.cpp with main entry point

---
+++ lab4/LoginForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[System::STAThreadAttribute]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	lab4::MyForm form;
+	Application::Run(gcnew lab4::MyForm());
+	return 0;
+}
